b337b679dad824cc7e428db12754bffea8659d2b
ny030303 <51257615+ny030303@users.noreply.github.com>
2025-09-26 16:48:56 +0900

    chore: Restructure 이후 이전 루트 파일 삭제

--- src/constants/index.jsx
+++
@@ -1,5 +0,0 @@
-export const TIMER_CONFIG = {
-    focus: { duration: 25 * 60, label: "Focus" },
-    shortBreak: { duration: 5 * 60, label: "Short Break" },
-    longBreak: { duration: 15 * 60, label: "Long Break" },
-};
--- src/store/userStore.jsx
+++
@@ -1,8 +0,0 @@
-import create from 'zustand';
-
-export const useUserStore = create((set) => ({
-  isLoggedIn: false,
-  userProfile: null,
-  login: (profile) => set({ isLoggedIn: true, userProfile: profile }),
-  logout: () => set({ isLoggedIn: false, userProfile: null }),
-}));
