eea7880876e826d7a548d214adb68a7d7a9aae2a
ny030303 <51257615+ny030303@users.noreply.github.com>
2025-10-17 13:22:35 +0900

    로그인화면 수정

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -7,13 +7,21 @@ import MyScene from '../components/common/MyScene';
 // 지갑 연결 상태는 App.jsx에서 전역으로 관리되므로 별도의 props가 필요 없습니다.
 export default function LoginPage() {
     return (
-        <div className="w-full min-h-screen flex flex-col justify-center items-center text-center p-4 bg-gray-100 dark:bg-gray-900">
-            
+        <div className="w-full min-h-screen flex flex-col justify-center items-center text-center p-4 bg-gray-100 dark:bg-gray-900" 
+        style={{backgroundImage: "url('/images/background1.png')", backgroundSize: 'cover'}}>
+            {/* #3c4758 */}
             {/* <MyScene /> */}
-            <div className="relative p-8 sm:p-12 rounded-3xl max-w-md w-full bg-white dark:bg-slate-800 shadow-2xl">
+            <div className="relative w-full" style={{maxWidth:"34em"}}>
                 
                 {/* 로고와 제목 */}
-                <div className="mb-8">
+                <h1 className="text-3xl md:text-4xl font-bold text-gray-900 dark:text-white mb-4">
+                   Efforia
+                </h1>
+                
+                <p className="text-lg text-gray-600 dark:text-gray-300 mb-8">
+                   집중의 가치를 발견하고, 키워나가세요!
+                </p>
+                <div className="">
                     {/* <MyCharacter/> */}
                     {/* SVG 그래디언트 정의 */}
                     <svg width="0" height="0" style={{ position: 'absolute' }}>
@@ -24,25 +32,14 @@ export default function LoginPage() {
                             </linearGradient>
                         </defs>
                     </svg>
-                    {/* 로고 아이콘 */}
-                    <svg className="w-24 h-24 mx-auto" viewBox="0 0 24 24" fill="none" stroke="url(#logoGradient)" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
-                        <path d="M21 12a9 9 0 1 1-6.219-8.56"/>
-                        <path d="M3 12a9 9 0 0 0 11.23 8.814"/>
-                        <path d="M12 3a9 9 0 0 1 5.766 15.82"/>
-                    </svg>
+                    <img src="/images/backModels1.png" alt="bg" className='mb-12' />
                 </div>
                 
-                <h1 className="text-3xl md:text-4xl font-bold text-gray-900 dark:text-white mb-4">
-                    Green Energy REC Platform
-                </h1>
                 
-                <p className="text-lg text-gray-600 dark:text-gray-300 mb-8">
-                    당신의 친환경 에너지를 블록체인에 증명하세요.
-                </p>
                 
                 {/* 메인 지갑 연결 버튼 */}
                 <div className="w-full">
-                    <WalletConnectButton />
+                    <WalletConnectButton  />
                 </div>
 
                 {/* 사용자 안내 문구 */}
